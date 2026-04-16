b2b7b434257d2752778bf65444ec33f15db4bdca
helenamartins252 <mariahelenahellena9@gmail.com>
2026-04-16 17:49:51 -0300

    Delete aula08 directory

--- aula08/java/ex4.java
+++
@@ -1,9 +0,0 @@
-public class ex4 {
-    public static void main(String[] args) {
-        for (int a=10; a>=0; a--){
-            System.out.print(a);
-        }
-        System.out.println("Lançar!");
-    }
-
-}
