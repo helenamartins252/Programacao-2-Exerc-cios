ee79a81f2694546da66298d04c0141072da8a1c8
helenamartins252 <mariahelenahellena9@gmail.com>
2026-04-16 17:48:28 -0300

    Add files via upload

---
+++ aula08/java/ex6.java
@@ -0,0 +1,10 @@
+public class ex6 {
+    public static void main(String[] args) {
+        String[] nomes= {"Ana", "Bruno", "Alê", "Aline", "Clara"};
+        for (String nome: nomes){
+            if (nome.startsWith("A")){
+                System.out.print(nome);
+            }
+        }
+    }
+}
